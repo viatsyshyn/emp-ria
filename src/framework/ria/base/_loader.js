e8858fb5079cc080c5f336da851911e9c854142c
Volodymyr Iatsyshyn <viatsyshyn@emp-studio.net>
2012-06-21 21:56:53 +0000

    updated base, loader added transports

--- src/framework/ria/base/_loader.js
+++ src/framework/ria/base/_loader.js
@@ -5,133 +5,66 @@
  * called only if all dependencies' callbacks are successful
  *
  * @author Volodymyr Iatsyshyn
- * @email viatsyshyn@hellowebapps.com
+ * @email viatsyshyn@emp-game.com
  * @date 03.05.11
  * @fileoverview _loader module
  */
 
-(function () {
+(function (ria) {
     "use strict";
 
-    var depsGraph = {};
-    var modulesState = {};
-
-    function ScriptTagTransport(src, callback) {
-        var script_tag = document.createElement('script');
-        script_tag.setAttribute('type', 'text/javascript');
-        script_tag.setAttribute('src', src);
-
-        //script_tag.onerror = function() { callback(false, Array.prototype.slice.call(arguments)); };
-
-        script_tag.onload = script_tag.onreadystatechange = function() {
-            if(!this.readyState
-                || this.readyState == "loaded"
-                || this.readyState == "complete") {
-
-                callback(true);
-            }
-        };
-
-        document.getElementsByTagName('head')[0].appendChild(script_tag);
-    }
-
-    function AjaxTransport(src, callback) {
-        var xhr;
-        if (window.XMLHttpRequest) {
-            try {
-                xhr = new XMLHttpRequest();
-            } catch (e){}
-        } else if (window.ActiveXObject) {
-            try {
-                xhr = new ActiveXObject('Msxml2.XMLHTTP');
-            } catch (e){
-                try {
-                    xhr = new ActiveXObject('Microsoft.XMLHTTP');
-                } catch (e){}
-            }
-        }
-
-        if (xhr) {
-            xhr.onreadystatechange = function () {
-                if (this.readyState == 4) {
-                    if (this.status == 200) {
-                        callback(true, this.responseText, this);
-                    } else {
-                        callback(false, this.status, this);
-                    }
-                }
-            };
-            xhr.open("GET", src, true);
-            xhr.send(null);
-        } else {
-            throw Error('Ajax not enabled');
-        }
-
-        xhr = null;
-    }
-
     var staticContent = {};
-    var customLoaders = {};
+    //noinspection JSUnusedGlobalSymbols
+    var customLoaders = {
+        json: function (uri, callback) {
+            Loader.staticContentTransport(uri, function(state, content) {
+                if (!state)
+                    callback(state, false);
+
+                try {
+                    callback(state, JSON.parse(content));
+                } catch (e) {
+                    setTimeout(function () { throw e; }, 1);
+                    callback(state, false);
+                }
+            })
+        },
+
+        html: function (uri, callback) {
+            Loader.staticContentTransport(uri, callback);
+        }
+    };
+
     var Loader = {
-        transport: ScriptTagTransport,
-        staticContentTransport: AjaxTransport,
+        transport: ria.__API.transports.ScriptTagTransport,
+        staticContentTransport: ria.__API.transports.AjaxTransport,
 
         load: function (src, callback) {
             var uri = src + (/\?/.test(src) ? '&' : '?') + '_=' + Math.random().toString(36).substr(2);
-            isSystemReady = false;
-            if (/\.json$/.test(src))
-                return this.staticContentTransport(uri, function(state, content) {
-                    if (!state)
-                        callback(src, false);
-                    
-                    try {
-                        staticContent[src] = JSON.parse(content);
-                        callback(src, true);
-                    } catch (e) {
-                        setTimeout(function () { throw e; }, 1);
-                        callback(src, false);
-                    }
-                });
+            ria.__API._loader.isReady = false;
 
-            if (/\.html$/.test(src))
-                return this.staticContentTransport(uri, function(state, content) {
-                    if (!state)
-                        callback(src, false);
+            for(var ext in customLoaders) if (customLoaders.hasOwnProperty(ext)) {
+                if (new RegExp('\\.' + ext + '$').test(src)) {
+                    return customLoaders[ext](uri, function(state, content) {
+                        if (!state)
+                            callback(src, false);
 
-                    try {
-                        staticContent[src] = content;
-                        callback(src, true);
-                    } catch (e) {
-                        setTimeout(function () { throw e; }, 1);
-                        callback(src, false);
-                    }
-                });
-
-            for(var ext in customLoaders) {
-                if (customLoaders.hasOwnProperty(ext)) {
-                    var regex = new RegExp('\\.' + ext + '$');
-                    if (regex.test(src)) {
-                        return customLoaders[ext](uri, function(state, content) {
-                            if (!state)
-                                callback(src, false);
-
-                            try {
-                                staticContent[src] = content;
-                                callback(src, true);
-                            } catch (e) {
-                                setTimeout(function () { throw e; }, 1);
-                                callback(src, false);
-                            }
-                        });
-                    }
+                        try {
+                            staticContent[src] = content;
+                            callback(src, true);
+                        } catch (e) {
+                            setTimeout(function () { throw e; }, 1);
+                            callback(src, false);
+                        }
+                    });
                 }
             }
 
-            return this.transport(src, callback.bind(hwa.global, src));
+            return this.transport(src, callback.bind(ria.global, src));
         },
 
         resolve: function resolve(name) {
-            return name.match(/$http:\/\//) ? name : hwa.BASE_MODULE_URL + name;
+            return name.match(/^http:\/\//) ? name : ria.BASE_MODULE_URL + name;
         },
 
         getStaticContent: function (src) {
@@ -305,18 +238,13 @@
         });
 
         if (is_all_executed) {
-            isSystemReady = true;
-            checkReady();
+            ria.__API._loader.isReady = true;
+            ria.ready(ria.__EMPTY);
         }
     }
 
-    /**
-     * Resolves dependencies
-     *
-     * @param {Array} deps
-     * @param {Function} callback
-     */
-    window.hwa.__API._loader = {
+    //noinspection JSUnusedGlobalSymbols
+    ria.__API._loader = {
         getCallerModuleDescriptor: function () {
             var root = ModuleDescriptor.getById(getCallerRootModuleId());
             if ((root.isLoading() || root.isNotLoaded()))
@@ -344,111 +272,4 @@
             return Loader.addCustomLoader(ext, loader);
         }
     };
-
-    /**
-     * Empty fn
-     */
-    //hwa.emptyFn = function () {};
-
-    hwa.isBrowser = !!(typeof window !== "undefined" && navigator && document);
-    hwa.isWebWorker = !hwa.isBrowser && typeof importScripts !== "undefined";
-
-    if (hwa.isBrowser) {
-        var current = hwa.global.location.toString();
-        for(var i = current.length; i > 0; i--) {
-            if (current.charAt(i - 1) == '/') {
-                hwa.BASE_MODULE_URL = current.substr(0, i);
-                break;
-            }
-        }
-    }
-
-    /**
-     * Fires callback when DOM and all dependencies are ready
-     * @param {Function} callback
-     */
-
-    var readyCallbacks = [];
-    var isPageReady = !hwa.isBrowser;
-    var isSystemReady = true;
-
-    /* THE FOLLOWING CODE BLOCK IS PORTED FROM jQuery 1.5.1 */
-    hwa.isBrowser && (function () {
-        function onLoaded() {
-            isPageReady = true;
-            checkReady();
-        }
-
-        // Cleanup functions for the document ready method
-        var DOMContentLoaded;
-        if ( document.addEventListener ) {
-            DOMContentLoaded = function() {
-                document.removeEventListener( "DOMContentLoaded", DOMContentLoaded, false );
-                onLoaded();
-            };
-
-        } else if ( document.attachEvent ) {
-            DOMContentLoaded = function() {
-                // Make sure body exists, at least, in case IE gets a little overzealous (ticket #5443).
-                if ( document.readyState === "complete" ) {
-                    document.detachEvent( "onreadystatechange", DOMContentLoaded );
-                    onLoaded();
-                }
-            };
-        }
-
-        // Catch cases where $(document).ready() is called after the
-        // browser event has already occurred.
-        if ( document.readyState === "complete" ) {
-            // Handle it asynchronously to allow scripts the opportunity to delay ready
-            return setTimeout(onLoaded, 1);
-        }
-
-        // Mozilla, Opera and webkit nightlies currently support this event
-        if ( document.addEventListener ) {
-            // Use the handy event callback
-            document.addEventListener( "DOMContentLoaded", DOMContentLoaded, false );
-
-            // A fallback to window.onload, that will always work
-            window.addEventListener( "load", onLoaded, false );
-
-        // If IE event model is used
-        } else if ( document.attachEvent ) {
-            // ensure firing before onload,
-            // maybe late but safe also for iframes
-            document.attachEvent("onreadystatechange", DOMContentLoaded);
-
-            // A fallback to window.onload, that will always work
-            window.attachEvent( "onload", onLoaded );
-
-            // If IE and not a frame
-            // continually check to see if the document is ready
-            /* CODE BLOCK IS COMMENTED, TODO: ENSURE THAT CODE BLOCK IS REQUIRED
-            var toplevel = false;
-
-            try {
-                toplevel = window.frameElement == null;
-            } catch(e) {}
-
-            if ( document.documentElement.doScroll && toplevel ) {
-                doScrollCheck();
-            }*/
-        }
-    })();
-    /* END OF PORTED BLOCK */
-
-    function checkReady() {
-        if (isPageReady && isSystemReady) {
-            while(readyCallbacks.length > 0)
-                readyCallbacks.shift().call(hwa.global);
-        }
-    }
-
-    hwa.ready = function ready(callback) {
-        if (isPageReady && isSystemReady) {
-            callback();
-        } else {
-            readyCallbacks.push(callback);
-        }
-    };
-})();
+})(ria, ria.global);
