19d6809ee7f2ed213c83fbc87d0062a13e8ec41e
OstapStasyuk <OstapStasyuk@gmail.com>
2012-09-26 22:02:45 +0000

    fixed enum and identifier, add tests for them

--- src/framework/ria/base/enum.js
+++ src/framework/ria/base/enum.js
@@ -20,10 +20,6 @@ ria.__API = ria.__API || {};
     function EnumDescriptor(enumClass, name) {
         this.enumClass = enumClass;
         this.name = name;
-
-        //#ifdef DEBUG
-            Object.freeze(this);
-        //#endif
     }
 
     ria.__API.EnumDescriptor = EnumDescriptor;
@@ -47,23 +43,11 @@ ria.__API = ria.__API || {};
      * @return {Function}
      */
     ria.__API.enum = function (enumClass, name) {
-        function EnumProxy() {
-            return new EnumInstance(EnumProxy.__META);
-        }
-
-        EnumProxy.__META = new EnumDescriptor(enumClass, name);
-
-        //#ifdef DEBUG
-            Object.freeze(EnumProxy);
-        //#endif
-        return EnumProxy;
+        enumClass.__META = new EnumDescriptor(enumClass, name);
+        return enumClass;
     };
 
     ria.__API.isEnum = function (value) {
-        if (typeof value === 'function') {
-            return value.__META instanceof EnumDescriptor;
-        }
-
-        return value instanceof EnumInstance;
+        return (value && value.__META) ? value.__META instanceof EnumDescriptor : false;
     }
 })();
--- src/framework/ria/base/identifier.js
+++ src/framework/ria/base/identifier.js
@@ -17,8 +17,8 @@ ria.__API = ria.__API || {};
      * @param {String} name
      * @constructor
      */
-    function IdentifierDescriptor(identClass, name) {
-        this.identClass = identClass;
+    function IdentifierDescriptor(identifierClass, name) {
+        this.identifierClass = identifierClass;
         this.name = name;
 
         //#ifdef DEBUG
@@ -29,41 +29,20 @@ ria.__API = ria.__API || {};
     ria.__API.IdentifierDescriptor = IdentifierDescriptor;
 
     /**
-     * @param {IdentifierDescriptor} meta
-     * @constructor
-     */
-    function IdentifierInstance(meta) {
-
-        this.__META = meta;
-
-        //#ifdef DEBUG
-        Object.freeze(this);
-        //#endif
-    }
-
-    /**
      * @param {Class} enumClass
      * @param {String} name
      * @return {Function}
      */
-    ria.__API.enum = function (enumClass, name) {
-        function IdentifierProxy() {
-            return new IdentifierInstance(EnumProxy.__META);
-        }
-
-        IdentifierProxy.__META = new IdentifierDescriptor(enumClass, name);
+    ria.__API.identifier = function (identifierClass, name) {
+        identifierClass.__META = new IdentifierDescriptor(identifierClass, name);
 
         //#ifdef DEBUG
-            Object.freeze(IdentifierProxy);
+            Object.freeze(identifierClass);
         //#endif
-        return IdentifierProxy;
+        return identifierClass;
     };
 
     ria.__API.isIdentifier = function (value) {
-        if (typeof value === 'function') {
-            return value.__META instanceof IdentifierDescriptor;
-        }
-
-        return value instanceof IdentifierInstance;
+        return value.__META instanceof IdentifierDescriptor;
     }
 })();
