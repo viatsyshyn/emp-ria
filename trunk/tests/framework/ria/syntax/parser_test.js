716758f0ea18df6623e76bce26be7d36e16565dc
viatsyshyn@emp.org.ua <viatsyshyn@emp.org.ua@c7befad7-21ac-1450-89fc-1c69743804ce>
2012-10-06 10:08:36 +0000

    parser tests

--- trunk/tests/framework/ria/syntax/parser_test.js
+++ trunk/tests/framework/ria/syntax/parser_test.js
@@ -1,16 +1,53 @@
 (function (ria) {
-    "use strict";
+    //"use strict";
 
     TestCase("ParserTestCase").prototype = {
         testParseMethod: function () {
 
-            var result = ria.__SYNTAX.parseMethod([
-                [String, Number, Boolean],
-                Object, function protected_(a, b, c) {}
-            ]);
+            var MyAnnotation = ria.__API.annotation('MyAnnotation', [], []);
+
+            //noinspection WithStatementJS
+            with (ria.__SYNTAX.Modifiers) {
+                var result = ria.__SYNTAX.parseMethod([
+                    [MyAnnotation],
+                    [String, Number, Boolean],
+                    FINAL, ABSTRACT, OVERRIDE, VOID, function protected_(a, b, c) {}
+                ]);
+            }
 
             assertNotUndefined(result);
             assertInstanceOf(ria.__SYNTAX.MethodDescriptor, result);
+
+            assertEquals('protected_', result.name);
+            assertUndefined(result.retType);
+            assertEquals([String, Number, Boolean], result.argsTypes);
+            assertEquals(['a', 'b', 'c'], result.argsNames);
+            assertEquals([MyAnnotation()], result.annotations);
+            assertFunction(result.body);
+            assertObject(result.flags);
+            assertFalse(results.flags.isPublic);
+            assertTrue(results.flags.isAbstract);
+            assertTrue(results.flags.isOverride);
+            assertTrue(results.flags.isFinal);
+        },
+
+        testParseProperty: function () {
+            var MyAnnotation = ria.__API.annotation('MyAnnotation', [], []);
+
+            //noinspection WithStatementJS
+            with (ria.__SYNTAX.Modifiers) {
+                var result = ria.__SYNTAX.parseProperty([
+                    [MyAnnotation],
+                    String, 'prop'
+                ]);
+            }
+
+            assertNotUndefined(result);
+            assertInstanceOf(ria.__SYNTAX.PropertyDescriptor, result);
+
+            assertEquals('prop', result.name);
+            assertEquals(String, result.retType);
+            assertEquals([MyAnnotation()], result.annotations);
         }
     };
 
