67d01b189840f7e1473d3b5cd16631281cf740ba
Volodymyr Iatsyshyn <viatsyshyn@emp-studio.net>
2012-10-07 06:29:36 +0000

    class builder

--- src/framework/ria/syntax/class.js
+++ src/framework/ria/syntax/class.js
@@ -6,5 +6,26 @@ ria.__SYNTAX = ria.__SYNTAX || {};
 (function () {
     "use strict";
 
+    ria.__SYNTAX.BuildClass = function () {
+        var args = [].slice.call(arguments);
 
+        var body = args.pop();
+        // TODO: check for IMPLEMENTS
+        // TODO: check for EXTENDS
+        var name = args.pop();
+        var flags = {
+            isFinal: false,
+            isAbstract: false
+        };
+
+        ria.__SYNTAX.parseModifiers(args, flags);
+
+        var annotations = ria.__SYNTAX.parseAnnotations(args);
+
+        function ClassProxy() { }
+
+        // TODO: build class def
+
+        return ClassProxy;
+    };
 })();
