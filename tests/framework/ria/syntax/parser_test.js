5afe4ede3e4ae2f2a91364d03d94a6cbdbf479a5
Volodymyr Iatsyshyn <viatsyshyn@emp-studio.net>
2012-10-07 06:17:54 +0000

    updated

--- tests/framework/ria/syntax/parser_test.js
+++ tests/framework/ria/syntax/parser_test.js
@@ -2,6 +2,22 @@
     //"use strict";
 
     TestCase("ParserTestCase").prototype = {
+
+        testParseAnnotations: function () {
+            var MyAnnotation = ria.__API.annotation('MyAnnotation', [], []);
+            var MyAnnotation2 = ria.__API.annotation('MyAnnotation2', [String, Boolean], ['a', 'b']);
+
+            var result = ria.__SYNTAX.parseAnnotations([
+                [MyAnnotation],
+                [MyAnnotation2('test', true)]
+            ]);
+
+            assertArray(result);
+            assertEquals(2, result.length);
+            assertTrue(ria.__API.isAnnotation(result[0]));
+            assertTrue(ria.__API.isAnnotation(result[1]));
+        },
+
         testParseMethod: function () {
 
             var MyAnnotation = ria.__API.annotation('MyAnnotation', [], []);
@@ -19,16 +35,28 @@
             assertInstanceOf(ria.__SYNTAX.MethodDescriptor, result);
 
             assertEquals('protected_', result.name);
-            assertUndefined(result.retType);
-            assertEquals([String, Number, Boolean], result.argsTypes);
-            assertEquals(['a', 'b', 'c'], result.argsNames);
-            assertEquals([MyAnnotation()], result.annotations);
             assertFunction(result.body);
+
+            assertUndefined(result.retType);
+
+            assertArray(result.argsTypes);
+            assertEquals(String, result.argsTypes[0]);
+            assertEquals(Number, result.argsTypes[1]);
+            assertEquals(Boolean, result.argsTypes[2]);
+
+            assertArray(result.argsNames);
+            assertEquals('a', result.argsNames[0]);
+            assertEquals('b', result.argsNames[1]);
+            assertEquals('c', result.argsNames[2]);
+
+            assertArray(result.annotations);
+            assertTrue(ria.__API.isAnnotation(result.annotations[0]));
+
             assertObject(result.flags);
-            assertFalse(results.flags.isPublic);
-            assertTrue(results.flags.isAbstract);
-            assertTrue(results.flags.isOverride);
-            assertTrue(results.flags.isFinal);
+            assertFalse(result.flags.isPublic);
+            assertTrue(result.flags.isAbstract);
+            assertTrue(result.flags.isOverride);
+            assertTrue(result.flags.isFinal);
         },
 
         testParseProperty: function () {
@@ -46,8 +74,83 @@
             assertInstanceOf(ria.__SYNTAX.PropertyDescriptor, result);
 
             assertEquals('prop', result.name);
-            assertEquals(String, result.retType);
-            assertEquals([MyAnnotation()], result.annotations);
+            assertEquals(String, result.type);
+            assertArray(result.annotations);
+            assertTrue(ria.__API.isAnnotation(result.annotations[0]));
+        },
+
+        testParseMembers: function () {
+            var MyAnnotation = ria.__API.annotation('MyAnnotation', [], []);
+
+            var result;
+            assertNoException(function () {
+                //noinspection WithStatementJS
+                with (ria.__SYNTAX.Modifiers) {
+                    result = ria.__SYNTAX.parseMembers([
+                        [MyAnnotation],
+                        Function, 'delegate',
+
+                        [MyAnnotation],
+                        [String, Number, Boolean],
+                        FINAL, VOID, function protected_(a, b, c) {}
+                    ]);
+                }
+            });
+
+            assertArray(result);
+            assertEquals(2, result.length);
+
+            assertInstanceOf(ria.__SYNTAX.PropertyDescriptor, result[0]);
+            assertInstanceOf(ria.__SYNTAX.MethodDescriptor, result[1]);
+        },
+
+        testParseMembersErrorDetection: function () {
+            var MyAnnotation = ria.__API.annotation('MyAnnotation', [], []);
+
+            assertException(function () {
+                //noinspection WithStatementJS
+                with (ria.__SYNTAX.Modifiers) {
+                    ria.__SYNTAX.parseMembers([
+                        [MyAnnotation],
+                        Function, 'delegate'
+
+                        [MyAnnotation],
+                        [String, Number, Boolean],
+                        FINAL, VOID, function protected_(a, b, c) {}
+                    ]);
+                }
+            }, 'Error');
+
+            assertException(function () {
+                //noinspection WithStatementJS
+                with (ria.__SYNTAX.Modifiers) {
+                    ria.__SYNTAX.parseMembers([
+                        [MyAnnotation],
+                        Function, 'delegate',
+
+                        [MyAnnotation]
+                        [String, Number, Boolean],
+                        FINAL, VOID, function protected_(a, b, c) {}
+                    ]);
+                }
+            }, 'Error');
+
+            assertException(function () {
+                //noinspection WithStatementJS
+                with (ria.__SYNTAX.Modifiers) {
+                    ria.__SYNTAX.parseMembers([
+                        [MyAnnotation],
+                        Function, 'delegate',
+
+                        [MyAnnotation],
+                        [String, Number, Boolean],
+                        FINAL, VOID, function protected_(a, b, c) {}
+
+                        [MyAnnotation],
+                        Function, 'delegate2'
+                    ]);
+                }
+            }, 'Error');
         }
     };
 
