b66c4a0538569d4c5bdf637b150f6501a9ade324
OstapStasyuk@gmail.com <OstapStasyuk@gmail.com@c7befad7-21ac-1450-89fc-1c69743804ce>
2012-10-11 21:18:08 +0000

    add annotation syntax implementation and tests for it

---
+++ trunk/tests/framework/ria/syntax/annotation_test.js
@@ -0,0 +1,86 @@
+(function (ria) {
+    "use strict";
+
+    TestCase("AnnotationTestCase").prototype = {
+        testBuildAnnotation: function () {
+            var annotation = ria.__SYNTAX.parseMethod([
+                [String, String],
+                function compare(_1, _2) {}
+            ]);
+
+            var result;
+            assertNoException(function() {
+                result = ria.__SYNTAX.buildAnnotation('Compare', annotation);
+            });
+
+            assertNotUndefined(result);
+            assertFunction(result);
+            assertTrue(ria.__API.isAnnotation(result));
+            assertEquals('Compare', result.__META.name);
+        },
+
+        testBuildAnnotationWithAnnotation: function () {
+
+            var MyAnnotation = ria.__API.annotation('MyAnnotation', [], []);
+
+            var annotation = ria.__SYNTAX.parseMethod([
+                [MyAnnotation],
+                [String, String],
+                function compare(_1, _2) {}
+            ]);
+
+            assertException(function() {
+                ria.__SYNTAX.buildAnnotation('Compare', annotation);
+            }, 'Error');
+        },
+
+        testBuildAnnotationWithFINAL: function () {
+
+            var annotation = ria.__SYNTAX.parseMethod([
+                [String, String],
+                ria.__SYNTAX.Modifiers.FINAL, function compare(_1, _2) {}
+            ]);
+
+            assertException(function() {
+                ria.__SYNTAX.buildAnnotation('Compare', annotation);
+            }, 'Error');
+        },
+
+        testBuildAnnotationWithAbstract: function () {
+
+            var annotation = ria.__SYNTAX.parseMethod([
+                [String, String],
+                ria.__SYNTAX.Modifiers.ABSTRACT, function compare(_1, _2) {}
+            ]);
+
+            assertException(function() {
+                ria.__SYNTAX.buildAnnotation('Compare', annotation);
+            }, 'Error');
+        },
+
+        testBuildAnnotationWithOverride: function () {
+
+            var annotation = ria.__SYNTAX.parseMethod([
+                [String, String],
+                ria.__SYNTAX.Modifiers.OVERRIDE, function compare(_1, _2) {}
+            ]);
+
+            assertException(function() {
+                ria.__SYNTAX.buildAnnotation('Compare', annotation);
+            }, 'Error');
+        },
+
+        testBuildAnnotationWithReturnType: function () {
+
+            var annotation = ria.__SYNTAX.parseMethod([
+                [String, String],
+                Boolean, function compare(_1, _2) {}
+            ]);
+
+            assertException(function() {
+                ria.__SYNTAX.buildAnnotation('Compare', annotation);
+            }, 'Error');
+        }
+    };
+
+})(ria);
