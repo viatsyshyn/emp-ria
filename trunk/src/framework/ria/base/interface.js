70842921081a088b036ac972de07033e14b7e60f
viatsyshyn@gmail.com <viatsyshyn@gmail.com@c7befad7-21ac-1450-89fc-1c69743804ce>
2012-10-11 06:27:41 +0000

    implemented ria.__API.ifc implemented ria.__SYNTAX.INTERFACE

---
+++ trunk/src/framework/ria/base/interface.js
@@ -0,0 +1,34 @@
+ria = ria || {};
+ria.__API = ria.__API || {};
+
+(function () {
+    "use strict";
+
+    /**
+     * @param {String} name
+     * @param {Array} methods
+     * @constructor
+     */
+    function InterfaceDescriptor(name, methods) {
+        this.name = name;
+        this.methods = methods;
+    }
+
+    ria.__API.InterfaceDescriptor = InterfaceDescriptor;
+
+    /**
+     * @param {String} name
+     * @param {Array} methods
+     * @return {Function}
+     */
+    ria.__API.ifc = function(name, methods) {
+        function InterfaceProxy() {
+            throw Error ('Can not instantiate interface');
+        }
+
+        InterfaceProxy.__META = new InterfaceDescriptor(name, methods);
+
+        return InterfaceProxy;
+    };
+
+})();
