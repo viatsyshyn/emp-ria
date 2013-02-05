d4e861e2d0ee0f01328d754dac39c9ea9637dc97
Volodymyr Iatsyshyn <viatsyshyn@emp-studio.net>
2013-02-05 22:01:34 +0000

    res-cl

---
+++ src/builder/jsbuild3.js
@@ -0,0 +1,12 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: paladin
+ * Date: 2/5/13
+ * Time: 8:01 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+function main() {
+    "use strict";
+
+}
