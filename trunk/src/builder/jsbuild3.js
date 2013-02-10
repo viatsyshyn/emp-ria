280d62b3b96c50969c7be4d68c1aba2cf16f3b2a
viatsyshyn@gmail.com <viatsyshyn@gmail.com@c7befad7-21ac-1450-89fc-1c69743804ce>
2013-02-10 19:48:10 +0000

    refactored jsbuild

--- trunk/src/builder/jsbuild3.js
+++ trunk/src/builder/jsbuild3.js
@@ -6,7 +6,51 @@
  * To change this template use File | Settings | File Templates.
  */
 
-function main() {
+Env.load('api/file.js');
+Env.load('api/json2.js');
+
+Env.load('jsbuild3/config.js');
+Env.load('jsbuild3/code-block.js');
+
+function main(configPath, jsonConfig) {
     "use strict";
 
+    try {
+        var externalConfig = jsonConfig ? JSON.parse(jsonConfig) : {};
+    } catch (e) {
+        throw new Error("Bad external config provided: " + jsonConfig);
+    }
+
+    try {
+        configPath = configPath || 'jsbuild.json';
+        configPath = File.absPath(File.exists(configPath) ? configPath : Env.CurrentDirectory + '/' + configPath);
+        var config = new Configuration(configPath, externalConfig);
+    } catch (e) {
+        throw new Error("Build file " + configPath + " is malformed: " + e);
+    }
+
+    config.getPlugins().forEach(/** @param {PluginConfiguration} plugin */ function (plugin) {
+        plugin.init();
+    });
+
+    config.getModules().forEach(/** @param {ModuleConfiguration} module */ function (module) {
+        config.setModuleConfig(module);
+
+        Env.print('Processing: ' + module.getName());
+        var inPath = config.resolveFilePath(module.getInFile());
+
+        var fileContents = [];
+        module.getPrepend().forEach(function (prepend) {
+            var path = config.resolveFilePath(prepend);
+            fileContents.push('/** @path ' + prepend + ' */');
+            fileContents.push(File.readFile(path));
+        });
+
+        fileContents.push(JsBuild.compile(inPath, config));
+
+        var outPath = File.absPath(config.getBasePath() + module.getOutFile());
+
+        var jsbuildParams = config.getPluginConfiguration("jsbuild");
+        File.saveUtf8File(outPath, fileContents.join(jsbuildParams.separator || ';'));
+    });
 }
