50cd8d12df6d38112edbf9c60e379d31c86aeca7
viatsyshyn@gmail.com <viatsyshyn@gmail.com@c7befad7-21ac-1450-89fc-1c69743804ce>
2013-06-01 17:54:45 +0000

    added ria.ajax sample implementation

--- trunk/src/framework/ria/async/Completer.js
+++ trunk/src/framework/ria/async/Completer.js
@@ -5,21 +5,23 @@ NAMESPACE('ria.async', function () {
 
     /** @class ria.async.Completer */
     CLASS(
-        'Completer', [
+        'Completer', IMPLEMENTS(ria.async.ICancelable), [
             READONLY, ria.async.Future, 'future',
+            READONLY, Boolean, 'complete',
 
             [[ria.async.ICancelable]],
             function $(canceler_) {
                 this.future = new ria.async.Future(canceler_);
+                this.complete = false;
             },
 
             [[String, Object]],
-            function doCallFuture_(method, arg) {
-                if (!this.future)
-                    return ;
+            function doCallFuture_(method, arg_) {
+                if (this.complete) return;
 
                 var future_protected = (this.future.__PROTECTED || this.future); // this is hack
-                return future_protected[method].call(future_protected, arg);
+                var args = []; arg_ !== undefined && args.push(arg_);
+                return future_protected[method].apply(future_protected, args);
             },
 
             VOID, function progress(data) {
@@ -27,11 +29,18 @@ NAMESPACE('ria.async', function () {
             },
 
             VOID, function complete(data) {
+                this.complete = true;
                 this.doCallFuture_('complete_', data);
             },
 
             VOID, function completeError(error) {
+                this.complete = true;
                 this.doCallFuture_('completeError_', error);
+            },
+
+            VOID, function cancel() {
+                this.complete = true;
+                this.doCallFuture_('completeBreak_');
             }
         ])
 });
