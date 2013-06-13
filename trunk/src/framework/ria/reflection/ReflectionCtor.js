9be730537ec93a3f36d75ece2abde54585e629e7
viatsyshyn@gmail.com <viatsyshyn@gmail.com@c7befad7-21ac-1450-89fc-1c69743804ce>
2013-06-13 12:13:29 +0000

    fixed ria.reflection

---
+++ trunk/src/framework/ria/reflection/ReflectionCtor.js
@@ -0,0 +1,39 @@
+REQUIRE('ria.reflection.Reflector');
+
+/** @namespace ria.reflection*/
+NS('ria.reflection', function () {
+    "use strict";
+
+    /** @class ria.reflection.ReflectionCtor */
+    CLASS(FINAL,
+        'ReflectionCtor', EXTENDS(ria.reflection.Reflector), [
+            READONLY, Function, 'clazz',
+            READONLY, String, 'name',
+
+            function $(clazz) {
+                VALIDATE_ARG('clazz', [ria.__API.ClassDescriptor], clazz.__META);
+                this.clazz = clazz;
+
+                this.method = clazz.__META.ctor;
+                this.name = 'ctor';
+            },
+
+            String, function getName() { return this.clazz.__META.name + '#' + this.name; },
+
+            Array, function getAnnotations() { return this.method.annotations; },
+
+            ArrayOf(String), function getArguments() { return this.method.argsNames;},
+            ArrayOf(String), function getRequiredArguments() {
+                return this.getArguments()
+                    .filter(function (_) { return !/^.+_$/.test(_) });
+            },
+
+            Array, function getArgumentsTypes() { return this.method.argsTypes;},
+
+            Boolean, function isAnnotatedWith(ann) {
+                return this.getAnnotations()
+                    .some(function (_) { return _.__META == ann.__META });
+            }
+        ]);
+});
+
