acb178853dad0c238ec9b8d238cdc28570fdeff7
viatsyshyn@gmail.com <viatsyshyn@gmail.com@c7befad7-21ac-1450-89fc-1c69743804ce>
2013-07-12 17:10:35 +0000

    lots of improvments on Activity flow

--- trunk/demos/framework/mvc-demo/app/controllers/NotesController.js
+++ trunk/demos/framework/mvc-demo/app/controllers/NotesController.js
@@ -66,10 +66,13 @@ NAMESPACE('app.controllers', function () {
                 var result = this.service
                     .getNotes()
                     //.then(function (data) { return ria.async.DeferredData(data, 10000)})
-                    .attach(this.validateResponse_());
+                    .attach(this.validateResponse_())
+                    .then(function (data) {
+                        return data.getItems()[0]
+                    });
 
                 /* Put activity in stack and render when result is ready */
-                return this.UpdateView(app.activities.Notes, result);
+                return this.UpdateView(app.activities.Notes, result, 'append');
             }
         ]);
 })
