5636054351f7db2608cc4d3bcfe8c713c1707da2
Volodymyr Iatsyshyn <viatsyshyn@emp-studio.net>
2013-08-14 23:06:30 +0000

    fixes to jsbuild3

---
+++ src/jsbuild3/lib/syntax/common.js
@@ -0,0 +1,9 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: C01t
+ * Date: 8/15/13
+ * Time: 12:03 AM
+ * To change this template use File | Settings | File Templates.
+ */
+
+var Exception = ria.__API.Exception;
