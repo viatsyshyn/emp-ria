cb755fbcb451c488d3c8302b80bea8dbfde42d07
viatsyshyn@gmail.com <viatsyshyn@gmail.com@c7befad7-21ac-1450-89fc-1c69743804ce>
2013-08-23 15:03:29 +0000

    added getService to context

--- trunk/src/framework/ria/mvc/BaseContext.js
+++ trunk/src/framework/ria/mvc/BaseContext.js
@@ -3,9 +3,17 @@ REQUIRE('ria.mvc.Dispatcher');
 
 NAMESPACE('ria.mvc', function () {
 
+    /** @class ria.mvc.ServiceCreateDelegate */
+    DELEGATE(
+        [[ClassOf(Class), ria.mvc.IContext]],
+        Class, function ServiceCreateDelegate(clazz, context) {});
+
     /** @class ria.mvc.BaseContext */
     CLASS(
         'BaseContext', IMPLEMENTS(ria.mvc.IContext), [
+
+            ria.mvc.ServiceCreateDelegate, 'serviceCreateDelegate',
+
             ria.mvc.IView, 'defaultView',
             ria.mvc.ISession, 'session',
             ria.mvc.Dispatcher, 'dispatcher',
@@ -17,6 +25,14 @@ NAMESPACE('ria.mvc', function () {
 
             ria.mvc.State, function getState() {
                 return this.dispatcher.getState();
+            },
+
+            [[ClassOf(Class)]],
+            Class, function getService(clazz) {
+                if (!this.serviceCreateDelegate)
+                    throw Exception('No service creator is provided');
+
+                return this.serviceCreateDelegate(clazz, this);
             }
         ]);
 });
