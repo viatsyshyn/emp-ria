cc4d1a395f89c22a9f55a20044a9a87195f32d13
viatsyshyn@gmail.com <viatsyshyn@gmail.com@c7befad7-21ac-1450-89fc-1c69743804ce>
2013-10-10 19:53:25 +0000

    added ria.mvc.SessionBind

--- trunk/src/framework/ria/mvc/Controller.js
+++ trunk/src/framework/ria/mvc/Controller.js
@@ -13,6 +13,7 @@ NAMESPACE('ria.mvc', function () {
 
     /** @class ria.mvc.ControllerUri */
     ANNOTATION(
+        [[String]],
         function ControllerUri(value) {});
 
     /** @class ria.mvc.AccessFor */
@@ -23,7 +24,10 @@ NAMESPACE('ria.mvc', function () {
     ANNOTATION(
         function Inject() {});
 
-
+    /** @class ria.mvc.SessionBind */
+    ANNOTATION(
+        [[String]],
+        function SessionBind(name_) {});
 
 
     function toCamelCase(str) {
@@ -41,13 +45,6 @@ NAMESPACE('ria.mvc', function () {
             ria.mvc.IContext, 'context',
             ria.mvc.State, 'state',
             ria.mvc.IView, 'view',
-
-            function $() {
-                BASE();
-                this.context = null;
-                this.state = null;
-                this.view = null;
-            },
 
             /**
              * Method is called once Application is starting
