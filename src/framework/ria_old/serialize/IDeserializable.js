e147160b4499fdd42fd070075432eee6117416d4
Volodymyr Iatsyshyn <viatsyshyn@emp-studio.net>
2013-10-15 08:40:32 +0000

    remove old version

--- src/framework/ria_old/serialize/IDeserializable.js
+++
@@ -1,9 +0,0 @@
-/** @namespace hwa.serialize */
-NAMESPACE('hwa.serialize', function () {
-    "use strict";
-
-    /** @class hwa.serialize.IDeserializable */
-    INTERFACE('IDeserializable', [
-        VOID, function deserialize(raw) {}
-    ])
-});
