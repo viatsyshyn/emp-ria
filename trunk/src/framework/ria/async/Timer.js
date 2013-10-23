9bd7c177f0f4815d0f0e162ada3f5d47a302437a
viatsyshyn@gmail.com <viatsyshyn@gmail.com@c7befad7-21ac-1450-89fc-1c69743804ce>
2013-10-23 14:13:07 +0000

    refactored activity-model-wait added scope to ria.async.Timer

--- trunk/src/framework/ria/async/Timer.js
+++ trunk/src/framework/ria/async/Timer.js
@@ -12,13 +12,13 @@ NAMESPACE('ria.async', function () {
     CLASS(
         'Timer', IMPLEMENTS(ria.async.ICancelable), [
             [[Number, ria.async.TimerDelegate]],
-            function $(duration, handler) {
+            function $(duration, handler, scope_) {
                 BASE();
                 var me = this;
                 var lastCall = new Date();
                 this.cleaner = clearInterval.bind(window);
                 this.timer = setInterval(function () {
-                    handler(me, -(lastCall.getTime() - (lastCall = new Date).getTime() ));
+                    handler.call(scope_, me, -(lastCall.getTime() - (lastCall = new Date).getTime() ));
                 }, duration < 0 ? 0 : duration);
             },
 
@@ -28,20 +28,20 @@ NAMESPACE('ria.async', function () {
             },
 
             [[Number, ria.async.TimerDelegate]],
-            function $once(duration, handler) {
+            function $once(duration, handler, scope_) {
                 BASE();
                 var me = this;
                 var lastCall = new Date();
                 this.cleaner = clearTimeout;
                 this.timer = setTimeout(function () {
                     this.timer = null;
-                    handler(me, -(lastCall.getTime() - (lastCall = new Date).getTime() ));
+                    handler.call(scope_, me, -(lastCall.getTime() - (lastCall = new Date).getTime() ));
                 }, duration < 0 ? 0 : duration);
             }
 
             /*
             [[ria.async.TimerDelegate, Array, Object]],
-            STATIC, VOID, function RUN(handler, args_, scope_) {
+            VOID, function RUN(handler, args_, scope_) {
                 ria.__API.defer(scope_ || window, handler, args_ || []);
             }
 
