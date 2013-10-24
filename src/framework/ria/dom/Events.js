c3ecd1b1effe024fd86fd56d5866d2b7f5f6f2cd
Volodymyr Iatsyshyn <viatsyshyn@emp-studio.net>
2013-10-24 10:07:23 +0000

    added ria.dom.Events

---
+++ src/framework/ria/dom/Events.js
@@ -0,0 +1,55 @@
+/**
+ * Created by viatsyshyn on 24.10.13.
+ */
+
+NAMESPACE('ria.dom', function () {
+    "use strict";
+
+    function def(data, def) {
+        return ria.__API.extendWithDefault(data || {}, def);
+    }
+
+    /** @class ria.dom.Events */
+    CLASS(
+        FINAL, 'Events', [
+            function CLICK(data_) {
+                return new MouseEvent('click', def(data_, {
+                    'view': window,
+                    'bubbles': true,
+                    'cancelable': true
+                }))
+            },
+
+            function FOCUS(data_) {
+                return new FocusEvent('focus', def(data_, {
+                    'view': window,
+                    'bubbles': true,
+                    'cancelable': true
+                }));
+            },
+
+            function CHANGE(data_) {
+                return new UIEvent('change', def(data_, {
+                    'view': window,
+                    'bubbles': true,
+                    'cancelable': true
+                }));
+            },
+
+            function KEY_UP(data_) {
+                return new KeyboardEvent('keyup', def(data_, {
+                    'view': window,
+                    'bubbles': true,
+                    'cancelable': true
+                }));
+            },
+
+            function KEY_DOWN(data_) {
+                return new KeyboardEvent('keydown', def(data_, {
+                    'view': window,
+                    'bubbles': true,
+                    'cancelable': true
+                }));
+            }
+        ])
+});
