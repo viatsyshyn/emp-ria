d6265d68356da2fbabcf012e338fea7f80660d3b
Volodymyr Iatsyshyn <viatsyshyn@emp-studio.net>
2013-12-08 17:24:09 +0000

    refactored jsbuild3 for NodeJS
    added grunt-jsbuild3 task

--- src/jsbuild3/index.js
+++ src/jsbuild3/index.js
@@ -1,1 +1,37 @@
-exports = require("./tools/jsbuild");
+var JsBuild3 = require("./tools/jsbuild"),
+    fs = require('fs');
+
+module.exports = function (configPath, modules) {
+    "use strict";
+
+    var CFG = new JsBuild3.Configuration(JSON.parse(fs.readFileSync(configPath)), configPath);
+
+    for (var plugin in CFG.getPlugins()) {
+        console.info(plugin.path);
+    }
+
+    var MODULES = modules;
+    var toBuild = CFG.getModules();
+    if (MODULES.length) {
+        MODULES = MODULES.filter(function (_) {
+            return _ != null;
+        });
+        toBuild = toBuild.filter(function (module) {
+            return MODULES.indexOf(module.getName()) >= 0;
+        });
+    }
+
+    toBuild.forEach(/** @param {ModuleConfiguration} module */ function (module) {
+        CFG.setModuleConfig(module);
+
+        console.info('Processing: ' + module.getName());
+
+        var fileContents = JsBuild3.compile(module.getInFile() || module.getAppClass(), CFG, module.getAppClass());
+        var outPath = Path.resolve(CFG.getBasePath() + module.getOutFile());
+
+        var jsbuildParams = CFG.getPluginConfiguration("jsbuild");
+        fs.writeFileSync(outPath, fileContents.join(jsbuildParams.separator || ';'), "utf-8");
+    });
+
+    console.info("done");
+};
