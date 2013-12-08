1571c7b77903d39b4f24efe0e965dbb8163c4f4c
Volodymyr Iatsyshyn <viatsyshyn@emp-studio.net>
2013-12-08 15:18:49 +0000

    refactored jsbuild3 for NodeJS

--- src/jsbuild3/lib/assets/jade.js
+++ src/jsbuild3/lib/assets/jade.js
@@ -6,7 +6,7 @@
  * To change this template use File | Settings | File Templates.
  */
 
-var Jade = require('jade');
+var Jade = require('emp.ria-jade');
 
 setFileHandler('jade', function JadeFileHandler(content, config, path) {
     return '__ASSETS["' + AssetsCache[path] + '"] = ' + Jade.compile(content, {client: true, compileDebug: false, self: true});
