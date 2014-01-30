47090c94a9370d81fdfbca8eaecbed57cfc48dd4
viatsyshyn@gmail.com <viatsyshyn@gmail.com@c7befad7-21ac-1450-89fc-1c69743804ce>
2014-01-30 10:27:27 +0000

    fixed ftp-deploy

--- trunk/src/ftp-deploy/tasks/ftp-deploy.js
+++ trunk/src/ftp-deploy/tasks/ftp-deploy.js
@@ -7,45 +7,56 @@ module.exports = function (grunt) {
 
   var ftp;
 
-  function doCwd(inPath, cb) {
+  function doEnsureFolder(inPath, cb) {
+    grunt.log.debug('Ensure folder ' + inPath);
     ftp.raw.cwd(inPath, function (err) {
       if(!err) {
+        grunt.log.debug('Folder ' + inPath + ' changed');
         return cb(null);
       }
 
-      return ftp.raw.mkd(inPath, function (err) {
+      doEnsureFolder(path.dirname(inPath), function (err) {
         if (err) {
-          grunt.log.error('Error creating new remote folder ' + inPath + ' --> ' + err);
           return cb(err);
         }
-
-        grunt.log.ok('New remote folder created ' + inPath.yellow);
-        return doCwd(inPath, cb);
+      
+        grunt.log.debug('Creating new remote folder ' + inPath);
+        return ftp.raw.mkd(inPath, function (err) {
+          err ? grunt.log.error('Error creating new remote folder ' + inPath + ' --> ' + err)
+              : grunt.log.ok('New remote folder created ' + inPath);
+              
+          return cb(err);
+        });
       });
     });
   }
 
   function doPutFile(inFilename, outFilename, done) {
-    ftp.put(inFilename + '', outFilename + '', function (err) {
-      err ? grunt.log.error('Cannot upload file: ' + inFilename + ' --> ' + err)
-          : grunt.log.ok('Uploaded file: ' + inFilename + ' to: ' + outFilename);
+    ftp.raw.size(outFilename, function (err) {
+      if (!err) {
+        grunt.log.debug('File exists: ' + outFilename)
+        done();
+        return;
+      }
+      
+      grunt.log.debug('Started file upload: ' + inFilename);
+      ftp.put(inFilename + '', outFilename + '', function (err) {
+        err ? grunt.log.error('Cannot upload file: ' + inFilename + ' --> ' + err)
+            : grunt.log.ok('Complete file upload: ' + outFilename);
 
-      done(err);
+        done(err);
+      });
     });
   }
 
   function doUploadEntity(entry, done) {
-    doCwd(path.dirname(entry.dest), function(err) {
-      err && grunt.warn('Could not switch to remote folder!');
-
-      doPutFile(entry.src, path.basename(entry.dest + ''), function (err) {
-        if (err) {
-          grunt.warn('Failed uploading files!');
-        }
-        done(null);
-      });
+    doPutFile(entry.src, entry.dest, function (err) {
+      if (err) {
+        grunt.warn('Failed uploading files!');
+      }
+      done(null);
     });
-  }
+  }  
 
   function getAuthByKey(inKey) {
     return grunt.file.exists('.ftppass') && grunt.file.readJSON('.ftppass')[inKey] || {};
@@ -81,14 +92,25 @@ module.exports = function (grunt) {
       ftp.auth(authVals.username, authVals.password, function (err) {
         err && grunt.warn('Authentication ' + err);
 
-        async.eachSeries(files, doUploadEntity, function () {
-          ftp.raw.quit(function (err) {
-            err ? grunt.log.error(err)
-                : grunt.log.ok('FTP upload done!');
-
-            done();
-          });
+        var folders = {};
+        files.forEach(function (entry) {
+          folders[path.dirname(entry.dest)] = true;
         });
+        
+        // todo verify folders
+        async.eachSeries(Object.getOwnPropertyNames(folders), doEnsureFolder, function () {
+          // folders varified
+          
+          // lets upaload files
+          async.eachSeries(files, doUploadEntity, function () {
+            ftp.raw.quit(function (err) {
+              err ? grunt.log.error(err)
+                  : grunt.log.ok('FTP upload done!');
+
+              done();
+            });
+          });
+        })
       });
     });
   });
